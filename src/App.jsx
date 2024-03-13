1e2a1e82597d3b00f0b96478c3bb310e902434a6
Marcelo Siqueira <marcelojobxt7@gmail.com>
2024-03-13 18:07:49 -0300

    :art: style: Added global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './assets/styles/global';
+import defaultTheme from './assets/styles/themes/default';
+
 function App() {
   return (
-    <h1>MyContatcs</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h1>MyContatcs</h1>
+    </ThemeProvider>
   );
 }
 
