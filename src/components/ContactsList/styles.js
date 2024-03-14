fa9252e12b0c2822a7115ded57d3ba6739f4f7a3
Marcelo Siqueira <marcelojobxt7@gmail.com>
2024-03-13 22:45:32 -0300

    :art: style: Creation of Contact Cards and their styles

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -11,7 +11,6 @@ export const Header = styled.header`
 
 
   strong {
-    color: #222;
     font-size: 24px;
   }
 
@@ -29,4 +28,75 @@ export const Header = styled.header`
       color: #FFF;
     }
   }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 8px;
+
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+    }
+
+    span {
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #FFF;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0,4);
+  padding: 16px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-bottom: 16px;
+  }
+
+  .info {
+    margin-bottom: 4px;
+
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        font-weight: bold;
+        text-transform: uppercase;
+        margin-left: 9px;
+        border-radius: 4px;
+        padding: 4px;
+        color: ${({ theme }) => theme.colors.primary.main};
+        background: ${({ theme }) => theme.colors.primary.lighter};
+      }
+    }
+
+    span {
+      display: block;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+  }
+
+  .actions {
+      display: flex;
+      align-items: center;
+
+      button {
+        background: transparent;
+        border: none;
+        margin-left: 8px;
+      }
+    }
 `;
