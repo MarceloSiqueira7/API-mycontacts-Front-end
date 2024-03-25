7c15811d1012b2ddaccf4deff1b6f67000727c1b
Marcelo Siqueira <marcelojobxt7@gmail.com>
2024-03-25 14:30:30 -0300

    :hammer: feat: Creation of pages and their routes

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,22 +1,25 @@
 import { ThemeProvider } from 'styled-components';
 
+import { BrowserRouter } from 'react-router-dom/';
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
-import ContactList from '../ContactsList';
 
 import { Container } from './styles';
+import Routes from '../../Routes';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Container>
-        <Header />
-        <ContactList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
